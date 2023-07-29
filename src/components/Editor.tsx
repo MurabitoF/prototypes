ef58a8edf562f56ee9d5c1829db853bd7956070a
MurabitoF <francomurabito@outlook.com>
2023-07-28 23:24:20 -0300

    add header to editors and new styles

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,18 +1,20 @@
-import {type Monaco} from '@monaco-editor/react/dist/index'
+import { type Monaco } from "@monaco-editor/react/dist/index";
 import { type LanguagesType } from "../types";
 import { emmetHTML, emmetCSS, emmetJSX } from "emmet-monaco-es";
-import {Editor as MonacoEditor} from "@monaco-editor/react";
+import { Editor as MonacoEditor } from "@monaco-editor/react";
 import { useDebounce } from "../hooks/useDebounce";
 import { useEditorStore } from "../state/store";
+import { Allotment } from "allotment";
+
 interface Props {
-  language: LanguagesType 
+  language: LanguagesType;
 }
 
-const Editor = ({language}: Props) => {
-  const editorProps = useEditorStore((state) => state.editorSettings)
-  const editorValue = useEditorStore((state) => state[language])
-  const updateContent = useEditorStore((state) => state.updateContent)
-  const debouncedOnChange = useDebounce(updateContent, 500)
+const Editor = ({ language }: Props) => {
+  const editorProps = useEditorStore((state) => state.editorSettings);
+  const editorValue = useEditorStore((state) => state[language]);
+  const updateContent = useEditorStore((state) => state.updateContent);
+  const debouncedOnChange = useDebounce(updateContent, 500);
 
   function handleEditorWillMount(monaco: Monaco) {
     emmetHTML(monaco);
@@ -20,17 +22,24 @@ const Editor = ({language}: Props) => {
     emmetJSX(monaco);
   }
 
-  console.log(editorProps);
-
   return (
-    <MonacoEditor 
-      defaultLanguage={language}
-      defaultValue={editorValue}
-      beforeMount={handleEditorWillMount}
-      onChange={(value) => debouncedOnChange(language, value)}
-      theme='vs-dark'
-      options={{...editorProps}}
-    />
+    <Allotment vertical separator={false} className="bg-medium">
+      <Allotment.Pane minSize={20} maxSize={20}>
+        <div className="flex-container">
+          <div>{language.toLocaleUpperCase()}</div>
+        </div>
+      </Allotment.Pane>
+      <Allotment.Pane>
+        <MonacoEditor
+          defaultLanguage={language}
+          defaultValue={editorValue}
+          beforeMount={handleEditorWillMount}
+          onChange={(value) => debouncedOnChange(language, value)}
+          theme="vs-dark"
+          options={{ ...editorProps }}
+        />
+      </Allotment.Pane>
+    </Allotment>
   );
 };
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,10 @@
-
 const Sidebar = () => {
   return (
-    <section style={{width: 70, height: "100vh", backgroundColor: "#282c34"}}>
-    
+    <section style={{ width: 70, height: "100vh" }} className="bg-dark">
+      Pty
     </section>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
+
